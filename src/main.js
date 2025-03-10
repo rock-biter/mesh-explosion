ac2a43a3b795a2c06a30e12eef9a28ea5fb2f0af
rock-biter <74297920+rock-biter@users.noreply.github.com>
2025-03-10 16:30:34 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,159 @@
+import './style.css'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/Addons.js'
+// __controls_import__
+// __gui_import__
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Pane } from 'tweakpane'
+import fragmentShader from './shaders/box/fragment.glsl'
+import vertexShader from './shaders/box/vertex.glsl'
+
+/**
+ * Debug
+ */
+// __gui__
+const config = {
+	progress: 0,
+	distance: 7,
+}
+const pane = new Pane()
+
+pane
+	.addBinding(config, 'progress', {
+		min: 0,
+		max: 1,
+		step: 0.01,
+	})
+	.on('change', (ev) => {
+		material.uniforms.uProgress.value = ev.value
+		console.log(ev.value)
+		box.visible = ev.value === 0
+	})
+
+pane
+	.addBinding(config, 'distance', {
+		min: 1,
+		max: 10,
+		step: 0.05,
+	})
+	.on('change', (ev) => {
+		material.uniforms.uDistance.value = ev.value
+	})
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+// scene.background = new THREE.Color(0xdedede)
+
+const material = new THREE.ShaderMaterial({
+	fragmentShader,
+	vertexShader,
+	uniforms: {
+		uProgress: { value: config.progress },
+		uDistance: { value: config.distance },
+	},
+})
+
+const box = new THREE.Mesh(new THREE.BoxGeometry(2.01, 2.01, 2.01), material)
+// box.position.set(0.5, 0, 0)
+scene.add(box)
+
+const gltfLoader = new GLTFLoader()
+gltfLoader.load('/models/prova-fraction.glb', (gltf) => {
+	console.log(gltf)
+
+	gltf.scene.traverse((el) => {
+		if (el instanceof THREE.Mesh) {
+			el.material = material
+		}
+	})
+
+	scene.add(gltf.scene)
+})
+
+/**
+ * render sizes
+ */
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+}
+
+/**
+ * Camera
+ */
+const fov = 60
+const camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.1)
+camera.position.set(4, 4, 8)
+camera.lookAt(new THREE.Vector3(0, 2.5, 0))
+
+/**
+ * Show the axes of coordinates system
+ */
+// __helper_axes__
+// const axesHelper = new THREE.AxesHelper(3)
+// scene.add(axesHelper)
+
+/**
+ * renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+	antialias: window.devicePixelRatio < 2,
+})
+document.body.appendChild(renderer.domElement)
+handleResize()
+
+/**
+ * OrbitControls
+ */
+// __controls__
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
+/**
+ * Three js Clock
+ */
+// __clock__
+// const clock = new THREE.Clock()
+
+/**
+ * frame loop
+ */
+function tic() {
+	/**
+	 * tempo trascorso dal frame precedente
+	 */
+	// const deltaTime = clock.getDelta()
+	/**
+	 * tempo totale trascorso dall'inizio
+	 */
+	// const time = clock.getElapsedTime()
+
+	// __controls_update__
+	controls.update()
+
+	renderer.render(scene, camera)
+
+	requestAnimationFrame(tic)
+}
+
+requestAnimationFrame(tic)
+
+window.addEventListener('resize', handleResize)
+
+function handleResize() {
+	sizes.width = window.innerWidth
+	sizes.height = window.innerHeight
+
+	camera.aspect = sizes.width / sizes.height
+
+	// camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix()
+
+	renderer.setSize(sizes.width, sizes.height)
+
+	const pixelRatio = Math.min(window.devicePixelRatio, 2)
+	renderer.setPixelRatio(pixelRatio)
+}
---
+++ src/shaders/box/fragment.glsl
@@ -0,0 +1,32 @@
+uniform float uProgress;
+
+varying vec3 vNormal;
+varying vec3 vWNormal;
+varying vec3 vWPosition;
+
+void main() {
+
+  float light = dot(normalize(vWPosition),normalize(vWNormal)); // -1 | 1.
+  light = min(light, 0.); // -1 | 0.
+  light = abs(light);
+  light = smoothstep(0.,0.4,light);
+  light *= 1. - smoothstep(0.,2.,length(vWPosition * uProgress * 0.5)) * 0.7;
+
+
+  vec3 dirLight = vec3(0.5,2.,1.);
+
+  vec3 baseColor = vec3(1.0,0.0,0.1) * clamp(0.,1.,dot(normalize(dirLight),normalize(vWNormal)));
+  baseColor = max(baseColor, vec3(0.05,0,0.05));
+  // baseColor = mix(baseColor,vec3(0,0.,0.),clamp(0.,0.8,length(vWPosition * uProgress * 0.5)));
+  vec3 lightColor = vec3(1.,1.,1.);
+  vec3 color = mix(baseColor, lightColor, light);
+
+  float fog = distance(vec3(0.0),vWPosition);
+  fog = smoothstep(2.,8.,fog);
+  color = mix(color, vec3(0),fog);
+
+  gl_FragColor = vec4(color,1.);
+
+  #include <tonemapping_fragment>
+	#include <colorspace_fragment>
+}
