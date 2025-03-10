872f85d33dcbbb88a6be7422766273706e4ab295
rock-biter <74297920+rock-biter@users.noreply.github.com>
2025-03-10 23:43:56 +0100

    add custom god ray effect

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { Pane } from 'tweakpane'
 import fragmentShader from './shaders/box/fragment.glsl'
 import vertexShader from './shaders/box/vertex.glsl'
+import gsap from 'gsap'
+import { lerp } from 'three/src/math/MathUtils.js'
 
 /**
  * Debug
@@ -26,9 +28,9 @@ pane
 		step: 0.01,
 	})
 	.on('change', (ev) => {
-		material.uniforms.uProgress.value = ev.value
-		console.log(ev.value)
-		box.visible = ev.value === 0
+		// gsap.to(material.uniforms.uProgress, { value: ev.value, duration: 0.5 })
+		// // console.log(ev.value)
+		// box.visible = ev.value === 0
 	})
 
 pane
@@ -59,6 +61,13 @@ const material = new THREE.ShaderMaterial({
 const box = new THREE.Mesh(new THREE.BoxGeometry(2.01, 2.01, 2.01), material)
 // box.position.set(0.5, 0, 0)
 scene.add(box)
+
+scene.add(
+	new THREE.Mesh(
+		new THREE.IcosahedronGeometry(0.3, 2),
+		new THREE.MeshBasicMaterial({ color: new THREE.Color(0.0, 0.0, 0.4) })
+	)
+)
 
 const gltfLoader = new GLTFLoader()
 gltfLoader.load('/models/prova-fraction.glb', (gltf) => {
@@ -103,7 +112,6 @@ const renderer = new THREE.WebGLRenderer({
 	antialias: window.devicePixelRatio < 2,
 })
 document.body.appendChild(renderer.domElement)
-handleResize()
 
 /**
  * OrbitControls
@@ -111,12 +119,80 @@ handleResize()
 // __controls__
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
+controls.autoRotate = true
+controls.autoRotateSpeed = 8
 
 /**
  * Three js Clock
  */
 // __clock__
-// const clock = new THREE.Clock()
+const clock = new THREE.Clock()
+
+const renderTarget = new THREE.WebGLRenderTarget(sizes.width, sizes.height, {
+	format: THREE.RGBAFormat,
+	stencilBuffer: false,
+	depthBuffer: true,
+})
+
+const RTScene = new THREE.Scene()
+const RTGeometry = new THREE.BufferGeometry()
+RTGeometry.setAttribute(
+	'position',
+	new THREE.BufferAttribute(new Float32Array([-1, -1, 3, -1, -1, 3]), 2)
+)
+
+const RTMaterial = new THREE.ShaderMaterial({
+	vertexShader: /* glsl */ `
+	void main() {
+		gl_Position = vec4(position.xy,1.0,1.0);
+	}
+	`,
+	fragmentShader: /* glsl */ `
+		uniform vec2 uResolution;
+		uniform sampler2D uScene;
+	
+		void main() {
+			vec2 uv = gl_FragCoord.xy / uResolution.xy;
+			vec2 texel = 1.0 / uResolution.xy;
+
+			vec3 lightColor = vec3(0., 0., 0.25) * 2.;
+
+			
+			vec3 color = texture(uScene,uv).rgb;
+			float div = 1.;
+			vec2 dir = normalize(vec2(0.5) - uv);
+
+			for(int i = 0; i < 40; i++) {
+				vec2 uvMap = uv + vec2(i) * texel * dir * 6.;
+				vec3 colorMap = texture(uScene,uvMap).rgb;
+				float f = max(0.0,0.6 - distance(lightColor,colorMap) - float(i) / 25.);
+				f = smoothstep(0.,1.,f);
+				color += colorMap * f;
+				div += f;
+			}
+
+			color /= div;
+
+  		float dith = sin(gl_FragCoord.x * 1.2) * 0.06 + cos(gl_FragCoord.y * 1.2) * 0.06;
+			// color += dith;
+
+
+			gl_FragColor = vec4(color,1.0);
+
+			#include <tonemapping_fragment>
+			#include <colorspace_fragment>
+		}
+	`,
+	uniforms: {
+		uResolution: { value: new THREE.Vector2(0) },
+		uScene: { value: renderTarget.texture },
+	},
+})
+
+const RTMesh = new THREE.Mesh(RTGeometry, RTMaterial)
+RTScene.add(RTMesh)
+
+handleResize()
 
 /**
  * frame loop
@@ -125,16 +201,27 @@ function tic() {
 	/**
 	 * tempo trascorso dal frame precedente
 	 */
-	// const deltaTime = clock.getDelta()
+	const dt = clock.getDelta()
 	/**
 	 * tempo totale trascorso dall'inizio
 	 */
 	// const time = clock.getElapsedTime()
 
 	// __controls_update__
-	controls.update()
+	controls.update(dt)
 
+	material.uniforms.uProgress.value = lerp(
+		material.uniforms.uProgress.value,
+		config.progress,
+		dt * 3
+	)
+
+	box.visible = material.uniforms.uProgress.value <= 0.01
+
+	renderer.setRenderTarget(renderTarget)
 	renderer.render(scene, camera)
+	renderer.setRenderTarget(null)
+	renderer.render(RTScene, camera)
 
 	requestAnimationFrame(tic)
 }
@@ -153,6 +240,9 @@ function handleResize() {
 	camera.updateProjectionMatrix()
 
 	renderer.setSize(sizes.width, sizes.height)
+	renderTarget.setSize(sizes.width, sizes.height)
+
+	renderer.getDrawingBufferSize(RTMaterial.uniforms.uResolution.value)
 
 	const pixelRatio = Math.min(window.devicePixelRatio, 2)
 	renderer.setPixelRatio(pixelRatio)
--- src/shaders/box/fragment.glsl
+++ src/shaders/box/fragment.glsl
@@ -18,12 +18,15 @@ void main() {
   vec3 baseColor = vec3(1.0,0.0,0.1) * clamp(0.,1.,dot(normalize(dirLight),normalize(vWNormal)));
   baseColor = max(baseColor, vec3(0.05,0,0.05));
   // baseColor = mix(baseColor,vec3(0,0.,0.),clamp(0.,0.8,length(vWPosition * uProgress * 0.5)));
-  vec3 lightColor = vec3(1.,1.,1.);
+  vec3 lightColor = vec3(0., 0.0, 0.25) * 2.;
   vec3 color = mix(baseColor, lightColor, light);
 
   float fog = distance(vec3(0.0),vWPosition);
   fog = smoothstep(2.,8.,fog);
   color = mix(color, vec3(0),fog);
+
+  float dith = sin(gl_FragCoord.x * 1.3) * 0.02 + cos(gl_FragCoord.y * 1.3) * 0.02;
+  color += mix(vec3(0),vec3(dith), 1. - fog);
 
   gl_FragColor = vec4(color,1.);
 
